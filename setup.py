af8a24bc8e15c84f52a837639a0075ba4c452b6a
Cayenne <cayennes@gmail.com>
2012-05-29 16:24:12 -0400

    Bump version to 0.5.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='KanjiColorizer',
     description='script and module to create colored stroke order '
         'diagrams based on KanjiVG data',
     long_description=open('README.rst').read(),
-    version='0.5.1dev',
+    version='0.5.1',
     author='Cayenne',
     author_email='cayennes@gmail.com',
     url='http://github.com/cayennes/kanji-colorize',
