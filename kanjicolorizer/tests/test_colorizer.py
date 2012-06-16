ef9eecc72a8dcdc498ed41c944b22eb8342464ad
Cayenne <cayennes@gmail.com>
2012-06-16 09:07:44 -0400

    Create files for unittest tests

---
+++ kanjicolorizer/tests/test_colorizer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: UTF-8 -*-
+
+# test_colorizer.py is part of kanji-colorize which makes KanjiVG data
+# into colored stroke order diagrams
+#
+# Copyright 2012 Cayenne Boyer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+import unittest
+from kanjicolorizer.colorizer import KanjiVG
+
+
+if __name__ == "__main__":
+    unittest.main()
